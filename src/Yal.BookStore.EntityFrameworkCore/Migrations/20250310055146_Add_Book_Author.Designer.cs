4bcd74731a9edaadb120b1aa0d0c57709888be8f
yangailin <2108119690@qq.com>
2025-03-10 13:54:53 +0800

    feat：Book和Author执行数据库迁移

--- src/Yal.BookStore.EntityFrameworkCore/Migrations/20250310055146_Add_Book_Author.Designer.cs
+++ src/Yal.BookStore.EntityFrameworkCore/Migrations/20250310055146_Add_Book_Author.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Yal.BookStore.EntityFrameworkCore;
 namespace Yal.BookStore.Migrations
 {
     [DbContext(typeof(BookStoreDbContext))]
-    partial class BookStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250310055146_Add_Book_Author")]
+    partial class Add_Book_Author
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Yal.BookStore.EntityFrameworkCore/Migrations/20250310055146_Add_Book_Author.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yal.BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Book_Author : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false, comment: "编码"),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false, comment: "名字"),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true, comment: "描述")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.Id);
+                },
+                comment: "作者");
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false, comment: "书名"),
+                    AuthorCode = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false, comment: "作者编码"),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true, comment: "描述")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                },
+                comment: "图书");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Author_Code",
+                table: "Author",
+                column: "Code");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorCode",
+                table: "Book",
+                column: "AuthorCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
